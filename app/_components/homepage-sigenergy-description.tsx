423101c5a3dd0b9a952147459281495ab57c9319
Philani Ncube <ncbphi001@gmail.com>
2023-12-03 21:26:06 +0200

    Add separators and utility pole icons to Sigenergy description

--- app/_components/homepage-sigenergy-description.tsx
+++ app/_components/homepage-sigenergy-description.tsx
@@ -1,4 +1,5 @@
-import { BatteryCharging, Grid3X3 } from "lucide-react";
+import { Separator } from "@radix-ui/react-separator";
+import { BatteryCharging, Grid3X3, UtilityPole } from "lucide-react";
 
 const SigenergDescriotion = () => {
   return (
@@ -25,31 +26,50 @@ const SigenergDescriotion = () => {
         <div className="grid grid-cols-1 mt-5 @lg:grid-cols-3 gap-8 text-teal-800">
           <div className="flex flex-col space-y-2 bg-teal-800 p-4 rounded-md text-white">
             <span className="flex gap-1 items-center">
-            <h3 className="text-xl font-medium">Storage Only </h3>
-            <BatteryCharging className="" />
+              <h3 className="text-xl font-medium">Storage Only </h3>
+              <Separator orientation="vertical" className="h-6" />
+              <BatteryCharging className="" />
+              <UtilityPole className="" />
             </span>
             <p className="text-sm leading-6">
-             Maximise your savings by storing energy in your Sigenergy battery systems from electricity from the grid. This energey can then be utilised either when there is a power outage or fill up the battery during off-peak hours when electicity is cheaper and then use the stored energy in the battery during peak hours when electricity is more expensive.
+              Maximise your savings by storing energy in your Sigenergy battery
+              systems from electricity from the grid. This energey can then be
+              utilised either when there is a power outage or fill up the
+              battery during off-peak hours when electicity is cheaper and then
+              use the stored energy in the battery during peak hours when
+              electricity is more expensive.
             </p>
           </div>
           <div className="flex flex-col space-y-2 bg-teal-800 p-4 rounded-md text-white">
             <span className="flex gap-1 items-center">
-            <h3 className="text-xl font-medium">Solar + Storage</h3>
-            <BatteryCharging className="" />
-            <Grid3X3 className="" />
+              <h3 className="text-xl font-medium">Solar + Storage</h3>
+              <Separator orientation="vertical" className="h-6" />
+              <BatteryCharging className="" />
+              <UtilityPole className="" />
+              <Grid3X3 className="" />
             </span>
             <p className="text-sm leading-6">
-           The SigenStar can be used to store energy from your solar panels and use it when the sun is not shining. This will allow you to maximise your savings by using your own solar energy instead of buying electricity from the grid. With an expertly designed solar system, you can reduce your electricity bill by up to 90%.
+              The SigenStar can be used to store energy from your solar panels
+              and use it when the sun is not shining. This will allow you to
+              maximise your savings by using your own solar energy instead of
+              buying electricity from the grid. With an expertly designed solar
+              system, you can reduce your electricity bill by up to 90%.
             </p>
           </div>
           <div className="flex flex-col space-y-2 bg-teal-800 p-4 rounded-md text-white">
             <span className="flex gap-1 items-center">
-            <h3 className="text-xl font-medium">Backup/Off-grid</h3>
-            <BatteryCharging className="" />
-            <Grid3X3 className="" />
+              <h3 className="text-xl font-medium">Backup/Off-grid</h3>
+              <Separator orientation="vertical" className="h-6" />
+              <BatteryCharging className="" />
+              <BatteryCharging className="" />
+              <Grid3X3 className="" />
             </span>
             <p className="text-sm leading-6">
-          In order to go completely Off-grid, you must have a battery system to store the energy from your solar panels. This will allow you to use your own solar energy during the day and at night. The SigenStar can also be used as a backup system to provide power during load shedding or power outages.
+              In order to go completely Off-grid, you must have a battery system
+              to store the energy from your solar panels. This will allow you to
+              use your own solar energy during the day and at night. The
+              SigenStar can also be used as a backup system to provide power
+              during load shedding or power outages.
             </p>
           </div>
         </div>
