055b41b43e8fe8ddbbe105dd5b96c40cc69754cc
Philani Ncube <ncbphi001@gmail.com>
2023-12-03 21:19:06 +0200

    Fix fontSans declaration in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 import DesktopNavigation from '@/components/navigation/desktop';
 
 // @ts-nocheck
-export const fontSans = FontSans({
+ const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
